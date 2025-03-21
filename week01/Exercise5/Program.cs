0eb6c89ade28e996fefde914665f6d90049a5f28
JoaquinECB <jcaceresbello@byui.edu>
2025-03-21 18:23:26 -0500

    favorite number

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static string userName;
+    static int favoriteNumber;
+    static int squaredNumber;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
+        SquareNumber();
+        DisplayResult();
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static void PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine(); 
+    }
+
+    static void PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        favoriteNumber = int.Parse(Console.ReadLine()); 
+    }
+
+    static void SquareNumber()
+    {
+        squaredNumber = favoriteNumber * favoriteNumber; 
+    }
+
+    static void DisplayResult()
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}.");
     }
 }
