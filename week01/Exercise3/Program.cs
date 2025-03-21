d15725ded26946686a6ec60fa720de58f8beabf6
JoaquinECB <jcaceresbello@byui.edu>
2025-03-21 17:44:41 -0500

    guess my number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 101);
+        int userGuess = 0;
+
+        Console.WriteLine("Welcome to 'Guess My Number'!");
+        Console.WriteLine("I have chosen a number between 1 and 100. Can you guess what it is?");
+
+        while (userGuess != numberToGuess)
+        {
+            Console.Write("Enter your guess: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out userGuess))
+            {
+                if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the number!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
+            }
+        }
     }
 }
