420850e7ecf902f3ba21b6bc471ac739e521e9ac
JoaquinECB <jcaceresbello@byui.edu>
2025-03-27 00:48:47 -0500

    jobs

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,15 @@
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"Job Title: {_jobTitle}");
+        Console.WriteLine($"Company: {_company}");
+        Console.WriteLine($"Start Year: {_startYear}");
+        Console.WriteLine($"End Year: {_endYear}");
+    }
+}
