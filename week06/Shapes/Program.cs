7b82a752532b1b096f15d3af38c50e1c6d878ce9
JoaquinECB <jcaceresbello@byui.edu>
2025-04-15 19:04:52 -0500

    shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Shape square = new Square("Red", 4);
+        Shape rectangle = new Rectangle("Blue", 5, 3);
+        Shape circle = new Circle("Green", 2.5);
+
+        Console.WriteLine($"Square (Color: {square.GetColor()}): Area = {square.GetArea()}");
+        Console.WriteLine($"Rectangle (Color: {rectangle.GetColor()}): Area = {rectangle.GetArea()}");
+        Console.WriteLine($"Circle (Color: {circle.GetColor()}): Area = {circle.GetArea()}");
     }
 }
